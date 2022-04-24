38c0751bd6856d07a5840d08cb2f5aa4c578521b
deepaksantoshtejwani <deepaktejwani13@gmail.com>
2022-04-24 20:33:30 +0530

    files and screens

---
+++ lib/appcolors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const backgroundTitle= Color.fromRGBO(62, 140, 132, 1);
+  static const buttonColor= Color.fromRGBO(15, 45, 64, 1);
+  static const yellow= Color.fromRGBO(255, 193, 0, 1);
+  static const textColor= Color.fromRGBO(216,242,240, 1);
+  AppColors();
+}
